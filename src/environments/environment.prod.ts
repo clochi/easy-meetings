58138bf800f02e64fa4ca11d5236b4357f0c2fca
Claudio Zurita <claudiozurita87@gmail.com>
2019-06-13 16:43:53 -0300

    Firebase hosting done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBtTi_0udF25_8H5T5OJWGjjGBCrXM9rBU",
+    authDomain: "easy-meetings.firebaseapp.com",
+    databaseURL: "https://easy-meetings.firebaseio.com",
+    projectId: "easy-meetings",
+    storageBucket: "easy-meetings.appspot.com",
+    messagingSenderId: "832213907163",
+    appId: "1:832213907163:web:8c205730fd464590"
+  }
 };
