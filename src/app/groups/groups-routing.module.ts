d8c536da1cbaf5831cf898f30dcd80a7897bb50a
Claudio Zurita <claudiozurita87@gmail.com>
2019-06-26 17:54:43 -0300

    My groups component, working

--- src/app/groups/groups-routing.module.ts
+++ src/app/groups/groups-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NoGroupComponent } from './no-group/no-group.component';
+import { MyGroupsComponent } from './my-groups/my-groups.component';
 
 const routes: Routes = [
-  { path: '' },
+  { path: '', component: MyGroupsComponent },
   { path: 'no-group', component: NoGroupComponent }
 ]
 
--- src/app/groups/groups.module.ts
+++ src/app/groups/groups.module.ts
@@ -5,9 +5,10 @@ import { GroupsRoutingModule } from './groups-routing.module';
 import { MaterialModule } from '../material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NewGroupFormComponent } from './new-group-form/new-group-form.component';
+import { MyGroupsComponent } from './my-groups/my-groups.component';
 
 @NgModule({
-  declarations: [NoGroupComponent, NewGroupFormComponent],
+  declarations: [NoGroupComponent, NewGroupFormComponent, MyGroupsComponent],
   imports: [
     CommonModule,
     FormsModule,
